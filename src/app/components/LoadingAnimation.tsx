d40b2e103aa8afc69967450e4c8521ade165a986
Sean Hall <seanhall4217@gmail.com>
2025-01-18 13:54:14 -0800

    bring loading animation closer to question text

--- src/app/components/LoadingAnimation.tsx
+++ src/app/components/LoadingAnimation.tsx
@@ -2,8 +2,8 @@ import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 export default function LoadingAnimation() {
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="w-32 h-32">
+    <div className="flex justify-center mt-4">
+      <div className="w-24 h-24">
         <DotLottieReact
           src="https://lottie.host/d6a8afbb-b7c1-4593-9b37-b50655f65790/eNKRMGiuLw.lottie"
           loop
